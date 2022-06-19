850d5952bba6c25124c879e081d5001fbbdd0ddf
Ishan Jaiswal <69758646+ishan-jaiswal-cse0247@users.noreply.github.com>
2022-06-19 12:24:17 +0530

    Add files via upload

---
+++ WG/resource/dashboard/prof_data/71ad9721062595d96a3b6375261068a8.php
@@ -0,0 +1,4 @@
+<div class="card_inner">
+            <p class="text-primary-p">Digital Marketing at amdocs</p>
+            <span class="text-muted text-title">Enrolled - 30 April 2021</span>
+          </div>
---
+++ WG/resource/dashboard/prof_data/e1ca8affb38225a77d0204497f4ffd3e.php
@@ -0,0 +1,8 @@
+<div class="card_inner">
+            <p class="text-primary-p">Graphic Designer at Studypal</p>
+            <span class="text-muted text-title">Enrolled - 1 May 2021</span>
+          </div>
+          <div class="card_inner">
+            <p class="text-primary-p">PHP Developer at Accenture</p>
+            <span class="text-muted text-title">Enrolled - 8 March 2021</span>
+          </div>
